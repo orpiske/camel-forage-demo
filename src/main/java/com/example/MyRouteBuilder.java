8c06dd1e087a07af47842e5b2435e0da09b700cf
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-08-11 13:47:27 +0200

    Adjust to use the refactored Header class

--- src/main/java/com/example/MyRouteBuilder.java
+++ src/main/java/com/example/MyRouteBuilder.java
@@ -1,7 +1,8 @@
 package com.example;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.langchain4j.agent.LangChain4jAgent;
+import org.apache.camel.component.langchain4j.agent.Headers;
+import org.apache.camel.component.langchain4j.agent.api.AiAgentBody;
 
 /**
  * A Camel Java DSL Router
@@ -13,15 +14,17 @@ public class MyRouteBuilder extends RouteBuilder {
      * Let's configure the Camel routing rules using Java code...
      */
     public void configure() {
+        AiAgentBody aiAgentBody = new AiAgentBody().withMemoryId(1);
+
         from("timer:startup?delay=2000&repeatCount=1")
                 .setBody(constant("My name is Alice"))
-                .setHeader(LangChain4jAgent.Headers.MEMORY_ID, constant(1))
+                .setHeader(Headers.MEMORY_ID, constant(1))
                 .to("langchain4j-agent:test-memory-agent?agentFactory=#class:org.apache.camel.forage.agent.factory.DefaultAgentFactory")
                 .log("${body}");
 
         from("timer:check?delay=5000&repeatCount=1")
                 .setBody(constant("What is my name?"))
-                .setHeader(LangChain4jAgent.Headers.MEMORY_ID, constant(1))
+                .setHeader(Headers.MEMORY_ID, constant(1))
                 .to("langchain4j-agent:test-memory-agent?agentFactory=#class:org.apache.camel.forage.agent.factory.DefaultAgentFactory")
                 .log("${body}");
     }
